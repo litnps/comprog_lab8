7549a23f844943041ffa1a8ab54147b1e5c213df
litnps <lalitth21.n@gmail.com>
2025-12-16 15:34:02 +0700

    Accepted : lab8_2.cpp

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -25,8 +25,10 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
